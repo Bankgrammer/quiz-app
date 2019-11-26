e6fe086c2dab0058d196026f5b14da73191c32b2
Thabang Moeketsi <codeconstable@gmail.com>
2019-11-26 23:53:32 +0200

    Intervals & Animating the Score

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
-const results = document.querySelector('.result');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-    // check the answers
-    userAnswers.forEach((answer, index) => {
-        if (answer === correctAnswers[index]) {
-            score += 25;
-        }
-    });
+  // check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]){
+      score += 25;
+    }
+  });
 
-    //Show results on page
-    scrollTo(0, 0);
-    results.querySelector('span').textContent = `${score}%`;
-    results.classList.remove('d-none');
+  // show the result
+  scrollTo(0, 0);
+  result.classList.remove('d-none');
 
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output === score){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 
 });
-
-
-// The window object
